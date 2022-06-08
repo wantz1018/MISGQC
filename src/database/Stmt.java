d05e3dcdc03c98940df385e5bc2d0d23326575b6
wantz <wantz@foxmail.com>
2022-06-08 16:04:53 +0800

    数据库操作

---
+++ src/database/Stmt.java
@@ -0,0 +1,28 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Stmt {
+    public static ResultSet getResultSet(String sql) throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            connection = DatabaseConnection.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+        return resultSet;
+    }
+}
